e1d9da5ef45f88964eb2c608fb76353fec85a7a9
EsmeVega <20191222@uthh.edu.mx>
2025-07-08 20:35:45 -0600

    subida citas

--- src/Components/Citas.jsx
+++ src/Components/Citas.jsx
@@ -209,6 +209,18 @@ function ViewVista() {
                 </form>
             </div>
           </div>
+      <div style={{ width: "100%", height: "400px" }}>
+      <iframe
+        title="Ubicación en Google Maps"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d10829.496609864684!2d-98.67107105000001!3d20.670868199999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses!2smx!4v1752025683683!5m2!1ses!2smx"
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+      </div>
           <Footer/>
     </div>
 
